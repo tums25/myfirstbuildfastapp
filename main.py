b85e9560dd7d4d99e119cffb2cce9f927d54c57e
tums25 <tumulroy@gmail.com>
2025-04-16 17:13:21 +0530

    tweet-news main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 
 # Create prompt template for generating tweets
 
-tweet_template = "Give me {number} tweets on {topic}"
+tweet_template = "Give me {number} latest news on {topic}"
 
 tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic'])
 
@@ -23,7 +23,7 @@ tweet_chain = tweet_prompt | gemini_model
 
 import streamlit as st
 
-st.header("Tweet Generator - SATVIK")
+st.header("Current News Generator")
 
 st.subheader("Generate tweets using Generative AI")
 
